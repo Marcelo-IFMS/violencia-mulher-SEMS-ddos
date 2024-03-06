06d15f70b0e2854a61d5ecfc1646e5f241adf11b
Marcelo Tomporoski Perez <marcelotperez@gmail.com>
2024-03-06 19:27:14 -0400

    Sistema de pontuação funcionando do Quiz

--- frontend/src/app/quiz/quiz.component.ts
+++ frontend/src/app/quiz/quiz.component.ts
@@ -12,6 +12,7 @@ export class QuizComponent implements OnInit {
   public questaocorrente: number = 0;
   public pontuacao: number = 0;
   public progress: string = "0";
+  public vetorPontuacao: any = [];
 
   constructor(private questionsService: QuestionService) { }
 
@@ -25,18 +26,35 @@ export class QuizComponent implements OnInit {
     })
 
   }
+  calcProgressBar() {
+    this.progress = ((((this.questaocorrente) + 1) / this.listaquestoes.length) * 100).toString();
+    if (this.questaocorrente === 0) {
+      this.progress = "0";
+    }
+  }
   nextQuestion() {
     this.questaocorrente++;
-    this.progress = ((((this.questaocorrente)+1) / this.listaquestoes.length) *100).toString();
-    
+    this.calcProgressBar();
+    // this.progress = ((((this.questaocorrente) + 1) / this.listaquestoes.length) * 100).toString();
   }
   previousQuestion() {
     this.questaocorrente--;
-    this.progress = ((((this.questaocorrente)+1) / this.listaquestoes.length) *100).toString();
-    if (this.questaocorrente ===0){
-      this.progress = "0";
-    }
-
+    this.calcProgressBar();
   }
-
+  addvetorPontuacao(pontos: any) {
+    this.vetorPontuacao[this.questaocorrente] = pontos;
+    this.pontuacao = this.vetorPontuacao.reduce((accumulator: any, value: any) => accumulator + value, 0);
+    console.log(this.listaquestoes.length);
+    console.log(this.questaocorrente);
+    if (!(this.listaquestoes.length === this.questaocorrente+1)){
+      this.nextQuestion();
+    }
+    
+  }
+  resetQuiz(){
+    this.pontuacao = 0;
+    this.questaocorrente = 0;
+    this.progress = "0";
+    this.vetorPontuacao = [];
+  }
 }
